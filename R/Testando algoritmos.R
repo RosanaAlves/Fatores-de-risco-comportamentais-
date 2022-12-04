da602511569a0aff6415e4f89f370094d956d3ca
RosanaAlves <rosanaalvesmolina7@gmail.com>
2022-12-04 09:44:26 -0300

    Update Testando algoritmos.R

--- R/Testando algoritmos.R
+++ R/Testando algoritmos.R
@@ -1,6 +1,7 @@
 #install.packages("caret")
 library(caret)
 
+CursoR::atualizar_material()
 indice_treino = createDataPartition(y=dados$Diabetes, p=0.7, list=FALSE)
 treino = dados[indice_treino, ]
 teste = dados[-indice_treino, ]
